af2e1c4a81f8dcb9716c2aeb0e68d3ec8b96b42c
Russell Heimlich <kingkool68@gmail.com>
2017-02-25 09:08:34 -0500

    Actually modify Vera's deets.

--- vera-heimlich/functions.php
+++ vera-heimlich/functions.php
@@ -1,10 +1,9 @@
 <?php
 // Constants
-define( 'CHILD_DATE_OF_BIRTH', '2017-02-24 11:34AM' );
-define( 'CHILD_NAME', 'Babby' );
-define( 'CHILD_INSTAGRAM_HANDLE', 'kingkool68' );
-define( 'CHILD_INSTAGRAM_HASHTAG', 'TheHeimlichs' );
-define( 'CHILD_FACEBOOK_URL', 'https://www.facebook.com/media/set/?set=ft.10101891838917048&type=1' );
-define( 'CHILD_THEME_COLOR', '#75ac6c' );
+define( 'CHILD_DATE_OF_BIRTH', '2017-02-24 12:13PM' );
+define( 'CHILD_NAME', 'Vera' );
+define( 'CHILD_INSTAGRAM_HANDLE', '' );
+define( 'CHILD_INSTAGRAM_HASHTAG', 'VeraAddison' );
+define( 'CHILD_FACEBOOK_URL', '' );
 
 include 'functions/scripts-styles.php';
