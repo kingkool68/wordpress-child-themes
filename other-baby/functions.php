0114c46a31d35bc22ac9b0432d8748c3ae6028c1
Russell Heimlich <kingkool68@gmail.com>
2017-02-25 23:44:50 -0500

    Remove unused files.

--- other-baby/functions.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Constants
-define( 'CHILD_DATE_OF_BIRTH', '2017-02-24 12:13PM' );
-define( 'CHILD_NAME', 'Vera' );
-define( 'CHILD_INSTAGRAM_HANDLE', '' );
-define( 'CHILD_INSTAGRAM_HASHTAG', 'VeraAddison' );
-define( 'CHILD_FACEBOOK_URL', '' );
-
-include 'functions/scripts-styles.php';
