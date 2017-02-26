d548fc0c8eb13bb2ee85d678c1643871a264a7e6
Russell Heimlich <kingkool68@gmail.com>
2017-02-25 23:47:08 -0500

    Add Vera's Facebook scrap book link.

--- vera-heimlich/functions.php
+++ vera-heimlich/functions.php
@@ -4,6 +4,6 @@ define( 'CHILD_DATE_OF_BIRTH', '2017-02-24 12:13PM' );
 define( 'CHILD_NAME', 'Vera' );
 define( 'CHILD_INSTAGRAM_HANDLE', '' );
 define( 'CHILD_INSTAGRAM_HASHTAG', 'VeraAddison' );
-define( 'CHILD_FACEBOOK_URL', '' );
+define( 'CHILD_FACEBOOK_URL', 'https://www.facebook.com/media/set/?set=ft.10103094755058458&type=1' );
 
 include 'functions/scripts-styles.php';
