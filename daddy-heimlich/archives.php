16666fbb97afea7f2f3449e68c8b8aae29795d2f
Russell Heimlich <kingkool68@gmail.com>
2017-03-20 00:06:54 -0400

    Render out the months.

--- daddy-heimlich/archives.php
+++ daddy-heimlich/archives.php
@@ -1,11 +1,11 @@
 <?php get_header(); ?>
 
 	<div id="content">
-		<section>
+		<section class="date-archive">
 			<h1>By Age</h1>
 		</section>
-		<section>
-			<h1>By Date</h1>
+		<section class="date-archive">
+			<h1 class="heading">By Date</h1>
 			<form>
 				<label>Enter a Date</label>
 				<input type="">
@@ -14,14 +14,19 @@
 			$data = get_monthly_archive_links();
 			foreach ( $data as $year => $month_data ) {
 				?>
-				<h2><a href="<?php echo esc_url( get_site_url() . '/' . $year . '/' ); ?>"><?php echo $year; ?></a></h2>
+				<h2 class="year-heading"><a href="<?php echo esc_url( get_site_url() . '/' . $year . '/' ); ?>"><?php echo $year; ?></a></h2>
 				<?php
-				foreach ( $month_data as $month => $obj ) {
-					$item = $month;
+				foreach ( $month_data as $month_num => $obj ) {
+					$month_abbr = date( 'M', strtotime( '2001-' . $month_num . '-01' ) );
+					$month_name = date( 'F', strtotime( '2001-' . $month_num . '-01' ) );
 					if ( ! empty( $obj ) ) {
-						$item = '<a href="' . esc_url( $obj->link ) . '" title="' . esc_attr( number_format( $obj->count ) ) . ' items">' . $item . '</a>';
+						$title_attr = 'View ' . $obj->count . ' items from ' . $month_name . ' ' . $year;
+						echo '<a href="' . esc_url( $obj->link ) . '" title="' . esc_attr( $title_attr ) . '" class="rounded-button item">';
+							echo '<span aria-label="' . esc_attr( $month_name . ' ' . $year ) . '">' . $month_abbr . '</span>';
+						echo '</a>';
+					} else {
+						echo '<span class="item rounded-button" aria-label="' . esc_attr( $month_name . ' ' . $year ) . '">' . $month_abbr . '</span>';
 					}
-					echo $item;
 				}
 			}
 			?>
