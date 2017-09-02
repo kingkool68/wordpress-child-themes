554db690d15fff3c822abac4fdb763485844613e
Russell Heimlich <kingkool68@gmail.com>
2017-09-02 00:30:04 -0400

    Fix date logic for the date 404 page.

--- daddy-heimlich/404-date-archive.php
+++ daddy-heimlich/404-date-archive.php
@@ -3,28 +3,38 @@ $daddio_dates = Daddio_Dates::get_instance();
 $year = get_query_var( 'year' );
 $month = get_query_var( 'monthnum' );
 $day = get_query_var( 'day' );
-$relative_date = new DateTime( $year . '-' . $month . '-' . $day );
+
+$future_prefix = 'On';
+
+if ( $year && ! $month && ! $day ) {
+	$timestamp = $year . '-01-01';
+	$time_format = 'Y';
+	$levels = 1;
+	$future_prefix = 'In';
+}
+if ( $year && $month && ! $day ) {
+	$timestamp = $year . '-' . $month . '-01';
+	$time_format = 'F, Y,';
+	$levels = 2;
+	$future_prefix = 'In';
+}
+if ( $year && $month && $day ) {
+	$timestamp = $year . '-' . $month . '-' . $day;
+	$time_format = get_option( 'date_format' ) . ',';
+	$levels = 3;
+}
+
+
+$relative_date = new DateTime( $timestamp );
+$tense = 'future';
+if ( $relative_date->format( 'U' ) < date( 'U' ) ) {
+	$tense = 'past';
+}
 $offset = $relative_date->format( 'U' ) - date( 'U' );
 $calculated_date = $offset + get_childs_birthday(); // integer since epoch
-$smallest_unit = $daddio_dates->get_smallest_time_unit( $year . '-' . $month . '-' . $day );
-$time_format = get_option( 'date_format' );
-switch( $smallest_unit ) {
-	case 'hour':
-	case 'hours':
-	case 'minute':
-	case 'minutes':
-	case 'second':
-	case 'seconds':
-		$time_format .= ' ' . get_option( 'time_format' );
-		break;
-}
+$smallest_unit = $daddio_dates->get_smallest_time_unit( $timestamp );
 
-$age = get_childs_birthday_diff( 2, $relative_date->format( 'U' ) );
-
-$tense = 'will be';
-if ( $relative_date->format( 'U' ) < date( 'U' ) ) {
-	$tense = 'was';
-}
+$age = get_childs_birthday_diff( $levels, $relative_date->format( 'U' ) );
 $date = $relative_date->format( $time_format );
 
 get_header();
@@ -32,7 +42,11 @@ get_header();
 	<div id="content">
 		<article class="article page">
 			<h1 class="title">Nothing Found!</h1>
-				<p>On <?php echo $date; ?> <?php echo CHILD_NAME ?> <?php echo $tense; ?> <?php echo $age; ?> old.</p>
+			<?php if ( 'past' == $tense ) : ?>
+					<p><?php echo $date; ?> was <?php echo $age; ?> before <?php echo CHILD_NAME ?>&rsquo;s birthday.</p>
+			<?php else : ?>
+				<p><?php echo $future_prefix; ?> <?php echo $date; ?> <?php echo CHILD_NAME ?> will be <?php echo $age; ?> old.</p>
+			<?php endif; ?>
 			<?php do_action( 'daddio_content_footer', $post ); ?>
 		</article>
 	</div>
