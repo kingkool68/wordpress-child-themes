a3a6aa045598b3c8808343d141a0ba03f09b7c1f
Russell Heimlich <kingkool68@gmail.com>
2017-12-09 01:10:40 -0500

    Add child theme color to Vera's site.

--- vera-heimlich/functions.php
+++ vera-heimlich/functions.php
@@ -5,5 +5,6 @@ define( 'CHILD_NAME', 'Vera' );
 define( 'CHILD_INSTAGRAM_HANDLE', '' );
 define( 'CHILD_INSTAGRAM_HASHTAG', 'VeraAddison' );
 define( 'CHILD_FACEBOOK_URL', 'https://www.facebook.com/media/set/?set=ft.10103094755058458&type=1' );
+define( 'CHILD_THEME_COLOR', '#75ac6c' );
 
 include 'functions/scripts-styles.php';
