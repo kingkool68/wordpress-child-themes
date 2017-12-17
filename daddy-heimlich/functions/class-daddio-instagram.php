8b4c944a99ab296bf7ab58c14ef7ed71a5c2a493
Russell Heimlich <kingkool68@gmail.com>
2017-12-17 12:43:30 -0500

    Add Location taxonomy

--- daddy-heimlich/functions/class-daddio-instagram.php
+++ daddy-heimlich/functions/class-daddio-instagram.php
@@ -74,6 +74,39 @@ class Daddio_Instagram {
 			'capability_type'     => 'page',
 		);
 		register_post_type( 'instagram', $args );
+
+		$labels = array(
+			'name'                       => 'Locations',
+			'singular_name'              => 'Location',
+			'menu_name'                  => 'Locations',
+			'all_items'                  => 'All Locations',
+			'parent_item'                => 'Parent Location',
+			'parent_item_colon'          => 'Parent Location:',
+			'new_item_name'              => 'New Location Name',
+			'add_new_item'               => 'Add New Location',
+			'edit_item'                  => 'Edit Location',
+			'update_item'                => 'Update Location',
+			'view_item'                  => 'View Location',
+			'separate_items_with_commas' => 'Separate locations with commas',
+			'add_or_remove_items'        => 'Add or remove locations',
+			'choose_from_most_used'      => 'Choose from the most used',
+			'popular_items'              => 'Popular Locations',
+			'search_items'               => 'Search Locations',
+			'not_found'                  => 'Not Found',
+			'no_terms'                   => 'No locations',
+			'items_list'                 => 'Locations list',
+			'items_list_navigation'      => 'Locations list navigation',
+		);
+		$args = array(
+			'labels'                     => $labels,
+			'hierarchical'               => true,
+			'public'                     => true,
+			'show_ui'                    => true,
+			'show_admin_column'          => true,
+			'show_in_nav_menus'          => true,
+			'show_tagcloud'              => true,
+		);
+		register_taxonomy( 'location', array( 'instagram' ), $args );
 	}
 
 	/**
