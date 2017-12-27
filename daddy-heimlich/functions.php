3d76333c162c44262f710d1efbeb91d4d77c47cb
Russell Heimlich <kingkool68@gmail.com>
2017-12-26 23:38:27 -0500

    Add a function for generating taxonomy labels.

--- daddy-heimlich/functions.php
+++ daddy-heimlich/functions.php
@@ -70,6 +70,37 @@ function ordinal_suffix( $n, $return_n = true ) {
 	return $return_n ? $n . $suffix : $suffix;
 }
 
+function daddio_generate_taxonomy_labels( $singular = '', $plural = '', $overrides = array() ) {
+	$lc_plural = strtolower( $plural );
+	$uc_plural = ucwords( $lc_plural );
+	$lc_singular = strtolower( $singular );
+	$uc_singular = ucwords( $lc_singular );
+
+	$labels = array(
+		'name'                       => $uc_plural,
+		'singular_name'              => $uc_singular,
+		'menu_name'                  => $uc_plural,
+		'all_items'                  => 'All ' . $uc_plural,
+		'parent_item'                => 'Parent ' . $uc_singular,
+		'parent_item_colon'          => 'Parent ' . $uc_singular . ':',
+		'new_item_name'              => 'New ' . $uc_singular . ' Name',
+		'add_new_item'               => 'Add New ' . $uc_singular,
+		'edit_item'                  => 'Edit ' . $uc_singular,
+		'update_item'                => 'Update ' . $uc_singular,
+		'view_item'                  => 'View ' . $uc_singular,
+		'separate_items_with_commas' => 'Separate ' . $lc_plural . ' with commas',
+		'add_or_remove_items'        => 'Add or remove ' . $lc_plural,
+		'choose_from_most_used'      => 'Choose from the most used',
+		'popular_items'              => 'Popular ' . $uc_plural,
+		'search_items'               => 'Search ' . $uc_plural,
+		'not_found'                  => 'Not Found',
+		'no_terms'                   => 'No ' . $lc_plural,
+		'items_list'                 => ucfirst( $lc_plural ) . ' list',
+		'items_list_navigation'      => ucfirst( $lc_plural ) . ' list navigation',
+	);
+	return wp_parse_args( $labels, $overrides );
+}
+
 // Include external libraries
 include 'vendor/ForceUTF8/Encoding.php';
 
