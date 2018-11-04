b8e8767554cf3bacbb70fe4a13c1fd5a49a4043b
Russell Heimlich <kingkool68@gmail.com>
2018-11-03 22:46:01 -0400

    Move debugging functions to their own file.

--- daddy-heimlich/functions.php
+++ daddy-heimlich/functions.php
@@ -1,33 +1,36 @@
 <?php
+
+// Include external libraries
+include 'vendor/ForceUTF8/Encoding.php';
+
+// Include our own libraries
+$files_to_require = array(
+	'debugging.php',
+	'class-daddio-admin.php',
+	'class-daddio-scripts-styles.php',
+	'class-daddio-dates.php',
+	'class-daddio-media.php',
+	'class-daddio-archive.php',
+	'class-daddio-menus.php',
+	'class-daddio-post-galleries.php',
+	'class-daddio-instagram.php',
+	'class-daddio-instagram-locations.php',
+	'rsvp.php',
+	'class-daddio-infinite-scroll.php',
+	'class-daddio-on-this-day.php',
+	'cli-commands.php',
+);
+foreach ( $files_to_require as $filename ) {
+	$file = get_template_directory() . '/functions/' . $filename;
+	if ( file_exists( $file ) ) {
+		require_once $file;
+	}
+}
+
 add_theme_support( 'post-thumbnails' );
 add_theme_support( 'automatic-feed-links' );
 add_theme_support( 'title-tag' );
 add_theme_support( 'html5', array( 'comment-list', 'comment-form', 'search-form', 'gallery', 'caption' ) );
-
-/**
- * Pretty-print alternative to var_dump()
- */
-if ( ! function_exists( 'wp_dump' ) ) :
-	function wp_dump() {
-		foreach ( func_get_args() as $arg ) {
-			echo '<xmp>';
-			var_dump( $arg );
-			echo '</xmp>';
-		}
-	}
-endif;
-
-if ( ! function_exists( 'wp_log' ) ) :
-	function wp_log() {
-		foreach ( func_get_args() as $arg ) {
-			if ( is_array( $arg ) || is_object( $arg ) ) {
-				error_log( print_r( $arg, true ) );
-			} else {
-				error_log( $arg );
-			}
-		}
-	}
-endif;
 
 // Google Analytics Debugging flag
 function daddio_google_analytics_debugging_flag( $class = array() ) {
@@ -98,31 +101,4 @@ function daddio_generate_taxonomy_labels( $singular = '', $plural = '', $overrid
 		'items_list_navigation'      => ucfirst( $lc_plural ) . ' list navigation',
 	);
 	return wp_parse_args( $labels, $overrides );
-}
-
-// Include external libraries
-include 'vendor/ForceUTF8/Encoding.php';
-
-// Include our own libraries
-$files_to_include = array(
-	'class-daddio-admin.php',
-	'class-daddio-scripts-styles.php',
-	'class-daddio-dates.php',
-	'class-daddio-media.php',
-	'class-daddio-archive.php',
-	'class-daddio-menus.php',
-	'class-daddio-post-galleries.php',
-	'class-daddio-instagram.php',
-	'class-daddio-instagram-locations.php',
-	'rsvp.php',
-	'class-daddio-infinite-scroll.php',
-	'class-daddio-on-this-day.php',
-	'cli-commands.php',
-);
-$dir = get_template_directory() . '/functions/';
-foreach ( $files_to_include as $filename ) {
-	$file = $dir . $filename;
-	if ( file_exists( $file ) ) {
-		include $file;
-	}
 }
