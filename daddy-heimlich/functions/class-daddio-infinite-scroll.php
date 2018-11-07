f76ec2ac4e1251414c3bb54377f70ef22e7d1706
Russell Heimlich <kingkool68@gmail.com>
2018-11-06 23:48:19 -0500

    Add docblocks.

--- daddy-heimlich/functions/class-daddio-infinite-scroll.php
+++ daddy-heimlich/functions/class-daddio-infinite-scroll.php
@@ -11,21 +11,33 @@ class Daddio_Infinite_Scroll {
 		static $instance = null;
 		if ( null === $instance ) {
 			$instance = new static();
-			$instance->setup_hooks();
+			$instance->setup_actions();
+			$instance->setup_filters();
 		}
 		return $instance;
 	}
 
-	public function setup_hooks() {
+	/**
+	 * Hook into WordPress via actions
+	 */
+	public function setup_actions() {
 		add_action( 'init', array( $this, 'action_init' ) );
 		add_action( 'pre_get_posts', array( $this, 'action_pre_get_posts' ) );
 		add_action( 'wp', array( $this, 'action_wp' ) );
+	}
 
+	/**
+	 * Hook into WordPress via filters
+	 */
+	public function setup_filters() {
 		add_filter( 'post_limits', array( $this, 'filter_post_limits' ) );
 		add_filter( 'redirect_canonical', array( $this, 'filter_redirect_canonical' ), 10, 2 );
 		add_filter( 'get_pagenum_link', array( $this, 'filter_get_pagenum_link' ) );
 	}
 
+	/**
+	 * Add the /pages/ rewrite endpoint and set up non-persistant caching groups
+	 */
 	public function action_init() {
 		// Make /pages/ rewrite rules work
 		add_rewrite_endpoint( 'pages', EP_ALL );
@@ -34,6 +46,11 @@ class Daddio_Infinite_Scroll {
 		wp_cache_add_non_persistent_groups( $this->cache_group );
 	}
 
+	/**
+	 * Modify the query if pages query var is set
+	 *
+	 * @param  WP_Query $query The WP_Query being run
+	 */
 	public function action_pre_get_posts( $query ) {
 		if ( is_admin() || ! $query->is_main_query() ) {
 			return;
@@ -51,6 +68,9 @@ class Daddio_Infinite_Scroll {
 		}
 	}
 
+	/**
+	 * Modify the paged value of global variables
+	 */
 	public function action_wp() {
 		global $wp_query, $paged;
 		$pages = $this->get_pages();
@@ -62,7 +82,11 @@ class Daddio_Infinite_Scroll {
 		$paged = get_query_var( 'paged' );
 	}
 
-	// Adjust the limit clause of the main SQL query
+	/**
+	 * Adjust the limit clause of the main SQL query
+	 * @param  string $limit Limit clause of SQL query
+	 * @return string        Modified limit clause
+	 */
 	public function filter_post_limits( $limit = '' ) {
 		global $wp_query;
 		if ( is_admin() || ! $wp_query->is_main_query() ) {
@@ -79,7 +103,14 @@ class Daddio_Infinite_Scroll {
 		return $limit;
 	}
 
-	// Kill the canonical 'paged' redirect if the pages query var is set. In other words we don't want /category/publications/?pages=1-10 to redirect to /category/publications/page/10/?pages=1-10 which is what would happen by default
+	/**
+	 * Kill the canonical 'paged' redirect if the pages query var is set.
+	 * In other words we don't want /category/publications/?pages=1-10 to redirect to /category/publications/page/10/?pages=1-10 which is what would happen by default
+	 *
+	 * @param  string $redirect_url  The URL WordPress is planning to redirect to
+	 * @param  string $requested_url The requested URL to WordPress
+	 * @return string                The modified redirect URL
+	 */
 	public function filter_redirect_canonical( $redirect_url = '', $requested_url = '' ) {
 		if ( get_query_var( 'pages' ) ) {
 			$redirect_url = trailingslashit( $requested_url );
@@ -87,6 +118,13 @@ class Daddio_Infinite_Scroll {
 		return $redirect_url;
 	}
 
+	/**
+	 * Strip out pages values from pagenum links
+	 * @TODO: Inspect this method more closely to see what it does
+	 *
+	 * @param  string $result The pagenum URL
+	 * @return string         The modified pagenum URL
+	 */
 	public function filter_get_pagenum_link( $result = '' ) {
 		if ( $pages = $this->get_pages() ) {
 			// Strip out anything that matches pages/{any digit or hypen or the word all}/
@@ -96,11 +134,11 @@ class Daddio_Infinite_Scroll {
 	}
 
 	/**
-	 * Helper Functions
+	 * Get various details about the paging logic for the current request
+	 *
+	 * @return object|false Details about the paging variables for the current request
 	 */
-
-	// Get various details about the paging logic for the current request
-	function get_pages() {
+	private function get_pages() {
 		$pages_var = get_query_var( 'pages' );
 		if ( ! $pages_var ) {
 			return false;
@@ -112,7 +150,7 @@ class Daddio_Infinite_Scroll {
 		}
 
 		$start = 1;
-		$end = false;
+		$end   = false;
 		$pages = explode( '-', $pages_var );
 
 		if ( count( $pages ) < 2 && isset( $pages[0] ) && ! empty( $pages[0] ) ) {
