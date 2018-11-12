2355e970426488364e88d8264bec6881164c935b
Russell Heimlich <kingkool68@gmail.com>
2018-11-11 23:39:50 -0500

    Add docblocks.

--- daddy-heimlich/functions/class-daddio-media.php
+++ daddy-heimlich/functions/class-daddio-media.php
@@ -7,14 +7,23 @@ class Daddio_Media {
 		static $instance = null;
 		if ( null === $instance ) {
 			$instance = new static();
-			$instance->setup_hooks();
+			$instance->setup_actions();
+			$instance->setup_filters();
 		}
 		return $instance;
 	}
 
-	public function setup_hooks() {
+	/**
+	 * Hook in to WordPress via actions
+	 */
+	public function setup_actions() {
 		add_action( 'init', array( $this, 'action_init' ) );
+	}
 
+	/**
+	 * Hook in to WordPress via filters
+	 */
+	public function setup_filters() {
 		add_filter( 'image_size_names_choose', array( $this, 'filter_image_size_names_choose' ) );
 		add_filter( 'img_caption_shortcode', array( $this, 'filter_img_caption_shortcode' ), 1, 3 );
 		add_filter( 'oembed_dataparse', array( $this, 'filter_oembed_dataparse' ), 10, 3 );
@@ -22,6 +31,9 @@ class Daddio_Media {
 		add_filter( 'shortcode_atts_video', array( $this, 'filter_shortcode_atts_video' ), 10, 4 );
 	}
 
+	/**
+	 * Setup custom image sizes
+	 */
 	public function action_init() {
 		//Custom image sizes
 		add_image_size( '320-wide', 320 );
@@ -31,13 +43,27 @@ class Daddio_Media {
 		add_image_size( '800-wide', 800 );
 	}
 
-	public function filter_image_size_names_choose( $sizes ) {
+	/**
+	 * Add image sizes to the list of image sizes in the media library
+	 *
+	 * @param  array  $sizes Sizes to add to the list
+	 * @return array         Modified list of sizes
+	 */
+	public function filter_image_size_names_choose( $sizes = array() ) {
 		$addsizes = array(
 			'350-wide' => '350 Wide',
 		);
 		return array_merge( $sizes, $addsizes );
 	}
 
+	/**
+	 * Make image captions use figure and figcaption markup and use aria attributes for accessibility
+	 *
+	 * @param  string $string  Original image caption
+	 * @param  array  $attr    Attributes
+	 * @param  string $content Content inside of the caption shortcode
+	 * @return string          Modified image caption
+	 */
 	function filter_img_caption_shortcode( $string = '', $attr = array(), $content = null ) {
 		$atts = shortcode_atts( array(
 			'id'    => '',
@@ -72,6 +98,14 @@ class Daddio_Media {
 			'</figure>';
 	}
 
+	/**
+	 * Wrap embeds in an element to make them responsive via CSS
+	 *
+	 * @param  string $return Embed code to return
+	 * @param  object $data   Data about the embed
+	 * @param  string $url    URL of the embed
+	 * @return string         Modified embed code
+	 */
 	public function filter_oembed_dataparse( $return = '', $data, $url ) {
 		$not_rich_embeds = array( 'https://twitter.com' );
 		switch ( $data->type ) {
@@ -85,17 +119,40 @@ class Daddio_Media {
 		return $return;
 	}
 
+	/**
+	 * Modify the URL of attachment links so they can be unique
+	 *
+	 * @param  string $link     Original attachment link
+	 * @param  integer $post_id Post ID of the attachemnt
+	 * @return string           New attachment link
+	 */
 	public function filter_attachment_link( $link, $post_id ) {
 		$post = get_post( $post_id );
 		$new_link = get_site_url() . '/attachment/' . $post->post_name . '/';
 		return $new_link;
 	}
 
-	public function filter_shortcode_atts_video( $out, $pairs, $atts, $shortcode ) {
+	/**
+	 * Modify shortcode attributes for [video]
+	 *
+	 * Settng preload to none improves load time performance
+	 *
+	 * @param  array $out       Shortcode attributes to modify
+	 * @return array            Modified shortcode attributes
+	 */
+	public function filter_shortcode_atts_video( $out ) {
 		$out['preload'] = 'none';
 		return $out;
 	}
 
+	/**
+	 * Display Instagram photo or video for a given Instagram post
+	 *
+	 * @TODO: Make this return a string 
+	 *
+	 * @param  integer $post_id Post ID of Instagram post
+	 * @return string           HTML of media
+	 */
 	public function the_instagram_media( $post_id = 0 ) {
 		$post_id = absint( $post_id );
 		if ( ! $post_id ) {
@@ -122,7 +179,7 @@ class Daddio_Media {
 
 		$img_attrs = array(
 			'class' => 'aligncenter from-instagram',
-			'alt' => '',
+			'alt'   => '',
 		);
 		echo wp_get_attachment_image( $featured_image_id, 'full', false, $img_attrs );
 	}
