a76878099e10711c02a887d1766b2ee437c91e9c
Russell Heimlich <kingkool68@gmail.com>
2018-11-27 00:40:16 -0500

    Twigify attachment pages.

--- daddy-heimlich/attachment.php
+++ daddy-heimlich/attachment.php
@@ -1,32 +1,54 @@
-<?php get_header(); ?>
+<?php
 
-	<div id="content">
-	<?php
-	if ( have_posts() ) :
-		while ( have_posts() ) : the_post();
-			$attachment_type = '';
-			if( $post->post_mime_type ) {
-				$pieces = explode('/', $post->post_mime_type);
-				$attachment_type = $pieces[0];
+$the_content = array();
+if ( have_posts() ) :
+	while ( have_posts() ) : the_post();
+	$post = get_post();
+	$attachment_type = '';
+	if ( ! empty( $post->post_mime_type ) ) {
+		$pieces = explode( '/', $post->post_mime_type );
+		if ( ! empty( $pieces[0] ) ) {
+			$attachment_type = $pieces[0];
+		}
+	}
+
+	$context   = array();
+	$templates = array();
+
+	switch ( $attachment_type ) {
+
+		case 'image' :
+
+			$attachment_size = get_query_var('size');
+			if ( ! $attachment_size || in_array( $attachment_size, array('full', 'original') ) ) {
+				$attachment_size = 'large';
 			}
-			?>
+			$img = wp_get_attachment_image_src( get_the_ID(), $attachment_size );
 
-			<?php do_action( 'daddio_attachment_before_article', $post ); ?>
+			$max_width = 'none';
+			if ( ! empty( $img[1] ) && is_numeric( $imag[1] ) ) {
+				$max_width = $img[1] / 16;
+			}
 
-			<article class="article attachment-<?php echo $attachment_type; ?>">
+			$context = array(
+				'daddio_content_header' => Sprig::do_action( 'daddio_content_header', $post ),
+				'the_title'             => get_the_title(),
+				'how_old'               => Daddio_Dates::how_old_was_child(),
+				'the_image'             => wp_get_attachment_image( get_the_ID(), $attachment_size ),
+				'max_width'             => $max_width,
+				'the_content'           => apply_filters( 'the_content', get_the_content() ),
+				'daddio_content_footer' => Sprig::do_action( 'daddio_content_footer', $post ),
+			);
+			$templates[] = 'attachment-image.twig';
+			break;
+	}
 
-				<?php do_action( 'daddio_attachment_before_template_part', $post ); ?>
-				<?php get_template_part( 'attachment', $attachment_type ); ?>
-				<?php do_action( 'daddio_attachment_after_template_part', $post ); ?>
+	$the_content[] =  Sprig::render( $templates, $context );
 
-			</article>
+	endwhile;
+endif;
 
-			<?php do_action( 'daddio_attachment_after_article', $post ); ?>
-
-			<?php
-		endwhile;
-	endif;
-	?>
-	</div>
-
-<?php get_footer(); ?>
+$context = array(
+	'the_content' => implode( "\n", $the_content ),
+);
+Sprig::out( 'attachment.twig', $context );
