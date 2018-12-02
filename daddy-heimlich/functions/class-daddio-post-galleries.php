9b56891fdf1dc19468d5a9ab08bedc525ca6d9b7
Russell Heimlich <kingkool68@gmail.com>
2018-12-02 00:11:48 -0500

    Finished adding docblocks.

--- daddy-heimlich/functions/class-daddio-post-galleries.php
+++ daddy-heimlich/functions/class-daddio-post-galleries.php
@@ -288,7 +288,8 @@ class Daddio_Post_Galleries {
 		foreach ( $matches[1] as $atts ) {
 			$atts      = shortcode_parse_atts( $atts );
 			$sort_args = wp_parse_args( $atts, $defaults );
-			if ( $haystack = $sort_args['ids'] ) {
+			if ( ! empty( $sort_args['ids'] ) ) {
+				$haystack = $sort_args['ids']
 				// Make it easier to match IDs while avoiding partial matches.
 				// Searching for "1" in "9,10,11" would be a false positive
 				// so we normalize everything with a trailing comma.
@@ -312,7 +313,7 @@ class Daddio_Post_Galleries {
 		);
 
 		$output = (object) array(
-			'parent_id' => $parent_post->ID,
+			'parent_id'   => $parent_post->ID,
 			'attachments' => array(),
 		);
 		foreach ( get_posts( $args ) as $post ) :
@@ -328,6 +329,11 @@ class Daddio_Post_Galleries {
 		return $output;
 	}
 
+	/**
+	 * Get information about the next / previous gallery items
+	 *
+	 * @return object Gallery post navigation data
+	 */
 	public function get_nav() {
 		// Inter request caching
 		if ( $output = wp_cache_get( $this->cache_key, $this->cache_group ) ) {
@@ -340,20 +346,22 @@ class Daddio_Post_Galleries {
 		}
 
 		$total_attachments = count( $posts->attachments );
-		$count = 0;
-		$current = $next = $prev = 0;
-		$post_id = get_the_ID();
-		$attachments = array();
+		$count             = 0;
+		$current           = $next = $prev = 0;
+		$post_id           = get_the_ID();
+		$attachments       = array();
 		foreach ( $posts->attachments as $attachment ) {
 			if ( $attachment->ID == $post_id ) {
 				$current = $count;
+
 				$next    = $count + 1;
+				if ( $next >= $total_attachments ) {
+					$next = 0;
+				}
+
 				$prev    = $count - 1;
 				if ( $prev < 0 ) {
 					$prev = $total_attachments - 1;
-				}
-				if ( $next >= $total_attachments ) {
-					$next = 0;
 				}
 			}
 
@@ -363,8 +371,8 @@ class Daddio_Post_Galleries {
 		}
 
 		$parent_permalink = get_permalink( $posts->parent_id );
-		$next_slug = $posts->attachments[ $next ]->post_name;
-		$prev_slug = $posts->attachments[ $prev ]->post_name;
+		$next_slug        = $posts->attachments[ $next ]->post_name;
+		$prev_slug        = $posts->attachments[ $prev ]->post_name;
 
 		$output = (object) array(
 			'attachments'    => $attachments,
@@ -378,6 +386,13 @@ class Daddio_Post_Galleries {
 		return $output;
 	}
 
+	/**
+	 * Get the post gallery permalink
+	 *
+	 * @param  integer $parent_id       ID of the parent post the gallery belongs to
+	 * @param  string  $attachment_slug Slug of the gallery item
+	 * @return string                   URL of post gallery permalink
+	 */
 	public function get_post_gallery_link( $parent_id = 0, $attachment_slug = '' ) {
 		if ( ! $parent_id || ! $attachment_slug ) {
 			return '';
@@ -385,10 +400,17 @@ class Daddio_Post_Galleries {
 		return get_permalink( $parent_id ) . 'gallery/' . $attachment_slug . '/';
 	}
 
+	/**
+	 * Format the post gallery output markup using HTML5 elements
+	 *
+	 * @param  string $nothing Nohing
+	 * @param  array  $attr    Attributes
+	 * @return string          HTML of the gallery rendered
+	 */
 	public function filter_post_gallery( $nothing, $attr = array() ) {
 		$post = get_post();
 
-		static $instance = 0;
+		$instance = 0;
 		$instance++;
 
 		if ( ! empty( $attr['ids'] ) ) {
@@ -463,9 +485,9 @@ class Daddio_Post_Galleries {
 			return $output;
 		}
 
-		$columns = intval( $atts['columns'] );
-		$img_size = $atts['size'];
-		$itemwidth = $columns > 0 ? floor( 100 / $columns ) : 100;
+		$columns    = intval( $atts['columns'] );
+		$img_size   = $atts['size'];
+		$itemwidth  = $columns > 0 ? floor( 100 / $columns ) : 100;
 
 		$size_class = sanitize_html_class( $atts['size'] );
 		$output = "<section class=\"gallery gallery-{$columns}-column gallery-size-{$img_size}\">";
@@ -482,7 +504,7 @@ class Daddio_Post_Galleries {
 			$img_width   = $image_meta['width'];
 			$img_height  = $image_meta['height'];
 			if ( isset( $image_meta['sizes'][ $atts['size'] ] ) ) {
-				$img_width = $image_meta['sizes'][ $atts['size'] ]['width'];
+				$img_width  = $image_meta['sizes'][ $atts['size'] ]['width'];
 				$img_height = $image_meta['sizes'][ $atts['size'] ]['height'];
 			}
 			$orientation = '';
@@ -516,6 +538,18 @@ class Daddio_Post_Galleries {
 		return $output;
 	}
 
+	/**
+	 * Our own version of get_attachment_link that uses the
+	 * Daddio Get Post Gallery Link method instead of get_attachment_link
+	 *
+	 * @param integer      $id        Post ID of the attachment
+	 * @param string|array $size      Image size. Accepts any valid image size, or an array of width and height values in pixels (in that order).
+	 * @param boolean      $permalink Whether to add permalink to image.
+	 * @param boolean      $icon      Whether the attachment is an icon.
+	 * @param boolean      $text      Link text to use. Activated by passing a string, false otherwise.
+	 * @param string|array $attr      Array or string of attributes.
+	 * @return string                 URL of the attachment
+	 */
 	public function get_attachment_link( $id = 0, $size = 'thumbnail', $permalink = false, $icon = false, $text = false, $attr = '' ) {
 		$id = intval( $id );
 		$_post = get_post( $id );
@@ -526,7 +560,7 @@ class Daddio_Post_Galleries {
 		}
 		if ( $permalink ) {
 			//$url = get_attachment_link( $_post->ID );
-			$url = daddio_get_post_gallery_link( $parent_post->ID, $_post->post_name );
+			$url = $this->get_post_gallery_link( $parent_post->ID, $_post->post_name );
 		}
 
 		if ( $text ) {
@@ -557,8 +591,3 @@ class Daddio_Post_Galleries {
 	}
 }
 Daddio_Post_Galleries::get_instance();
-
-function daddio_get_post_gallery_link( $parent_id = 0, $attachment_slug = '' ) {
-	$instance = Daddio_Post_Galleries::get_instance();
-	return $instance->get_post_gallery_link( $parent_id, $attachment_slug );
-}
