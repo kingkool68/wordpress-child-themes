5a9a61041866a25a1e7f888914089fe14a9bcda7
Russell Heimlich <kingkool68@gmail.com>
2018-12-06 22:34:19 -0500

    Twigify 404-age-archive.php

--- daddy-heimlich/404-age-archive.php
+++ daddy-heimlich/404-age-archive.php
@@ -9,11 +9,11 @@ if ( $age_query_var ) {
 	$age = preg_replace( '/(\d+)/', ' $1 ', $age_query_var );
 	$age = strtolower( $age ) . ' old';
 }
-$relative_date = new DateTime( $age_query_var );
-$offset = $relative_date->format( 'U' ) - date( 'U' );
+$relative_date   = new DateTime( $age_query_var );
+$offset          = $relative_date->format( 'U' ) - date( 'U' );
 $calculated_date = $offset + Daddio_Dates::get_childs_birthday(); // integer since epoch
-$smallest_unit = Daddio_Dates::get_smallest_time_unit( $age_query_var );
-$time_format = get_option( 'date_format' );
+$time_format     = get_option( 'date_format' );
+$smallest_unit   = Daddio_Dates::get_smallest_time_unit( $age_query_var );
 switch( $smallest_unit ) {
 	case 'hour':
 	case 'hours':
@@ -31,13 +31,10 @@ if ( $calculated_date < date( 'U' ) ) {
 }
 $date = date( $time_format, $calculated_date );
 
-get_header();
-?>
-	<div id="content">
-		<article class="article page">
-			<h1 class="title">Nothing Found!</h1>
-				<p class="date-explanation"><?php echo CHILD_NAME ?> <?php echo $tense; ?> <?php echo $age; ?> on <?php echo $date; ?></p>
-		</article>
-	</div>
-
-<?php get_footer();
+$context = array(
+	'child_name' => CHILD_NAME,
+	'tense'      => $tense,
+	'age'        => $age,
+	'date'       => $date,
+);
+Sprig::out( '404-age-archive.twig', $context );
