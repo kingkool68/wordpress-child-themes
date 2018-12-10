f39c017a2eee971ce59a7a91455e833cf6034c84
Russell Heimlich <kingkool68@gmail.com>
2018-12-09 23:35:46 -0500

    Basic styleguide is ready to go.
    Next step, adding content.

--- daddy-heimlich/functions.php
+++ daddy-heimlich/functions.php
@@ -43,3 +43,16 @@ function daddio_google_analytics_debugging_flag( $class = array() ) {
 	return $class;
 }
 add_filter( 'body_class', 'daddio_google_analytics_debugging_flag' );
+
+/**
+ * Add the styleguide directory to the known directories Sprig should look
+ * for Twig files to render
+ *
+ * @param  array  $paths Places Twig should look for Twig files
+ * @return array         Modified paths
+ */
+function filter_sprig_roots( $paths = array() ) {
+	$paths[] = get_template_directory() . '/styleguide';
+	return $paths;
+}
+add_filter( 'sprig/roots', 'filter_sprig_roots' );
