d9346abfdc8433e9a74ebf43f02a55eb33505dea
Russell Heimlich <kingkool68@gmail.com>
2019-05-16 10:35:45 -0400

    Add display=swap to Google Fonts
    Improves performance. See https://font-display.glitch.me/

--- vera-heimlich/functions/scripts-styles.php
+++ vera-heimlich/functions/scripts-styles.php
@@ -1,6 +1,6 @@
 <?php
 function vah_wp_enqueue_scripts() {
-	wp_register_style( 'vah-google-fonts', 'https://fonts.googleapis.com/css?family=Crete+Round:400,400italic|Shrikhand|Open+Sans:300italic,700italic,300,700', array(), null, 'all' );
+	wp_register_style( 'vah-google-fonts', 'https://fonts.googleapis.com/css?family=Crete+Round:400,400italic|Shrikhand|Open+Sans:300italic,700italic,300,700&display=swap', array(), null, 'all' );
 	wp_enqueue_style( 'vera-heimlich', get_stylesheet_directory_uri() . '/css/vera-heimlich' . Daddio_Scripts_Styles::get_css_suffix(), array( 'vah-google-fonts' ), null, 'all' );
 }
 add_action( 'wp_enqueue_scripts', 'vah_wp_enqueue_scripts' );
