db224e1bdd84fcacf08eff4b0e0a431a9a208c24
Russell Heimlich <kingkool68@gmail.com>
2019-07-20 23:01:24 -0400

    Add a post type label generating method

--- daddy-heimlich/functions/class-daddio-helpers.php
+++ daddy-heimlich/functions/class-daddio-helpers.php
@@ -44,7 +44,7 @@ class Daddio_Helpers {
 	}
 
 	/**
-	 * Simplify generating taxonomy labels by only needing to enter a singular and plural verison
+	 * Simplify generating taxonomy labels by only needing to enter a singular and plural version
 	 *
 	 * @param  string $singular  The singular version of the taxonomy label
 	 * @param  string $plural    The plural version of the taxonomy label
@@ -56,8 +56,7 @@ class Daddio_Helpers {
 		$uc_plural   = ucwords( $lc_plural );
 		$lc_singular = strtolower( $singular );
 		$uc_singular = ucwords( $lc_singular );
-
-		$labels = array(
+		$labels      = array(
 			'name'                       => $uc_plural,
 			'singular_name'              => $uc_singular,
 			'menu_name'                  => $uc_plural,
@@ -78,6 +77,51 @@ class Daddio_Helpers {
 			'no_terms'                   => 'No ' . $lc_plural,
 			'items_list'                 => ucfirst( $lc_plural ) . ' list',
 			'items_list_navigation'      => ucfirst( $lc_plural ) . ' list navigation',
+		);
+		return wp_parse_args( $labels, $overrides );
+	}
+
+	/**
+	 * Simplify generating post type labels by only needing to enter a singular and plural version
+	 *
+	 * @param  string $singular  The singular version of the post type label
+	 * @param  string $plural    The plural version of the post type label
+	 * @param  array  $overrides Specific labels to override that might not fit this pattern
+	 * @return array             Post type labels
+	 */
+	public static function generate_post_type_labels( $singular = '', $plural = '', $overrides = array() ) {
+		$lc_plural   = strtolower( $plural );
+		$uc_plural   = ucwords( $lc_plural );
+		$lc_singular = strtolower( $singular );
+		$uc_singular = ucwords( $lc_singular );
+		$labels      = array(
+			'name'                  => $uc_plural,
+			'singular_name'         => $uc_singular,
+			'menu_name'             => $uc_plural,
+			'name_admin_bar'        => $uc_singular,
+			'archives'              => $uc_singular . ' Archives',
+			'attributes'            => $uc_singular . ' Attributes',
+			'parent_item_colon'     => 'Parent ' . $uc_singular . ':',
+			'all_items'             => 'All ' . $uc_plural,
+			'add_new_item'          => 'Add New ' . $uc_singular,
+			'add_new'               => 'Add New',
+			'new_item'              => 'New ' . $uc_singular,
+			'edit_item'             => 'Edit ' . $uc_singular,
+			'update_item'           => 'Update ' . $uc_singular,
+			'view_item'             => 'View ' . $uc_singular,
+			'view_items'            => 'View ' . $uc_plural,
+			'search_items'          => 'Search ' . $uc_singular,
+			'not_found'             => 'Not found',
+			'not_found_in_trash'    => 'Not found in Trash',
+			'featured_image'        => 'Featured Image',
+			'set_featured_image'    => 'Set featured image',
+			'remove_featured_image' => 'Remove featured image',
+			'use_featured_image'    => 'Use as featured image',
+			'insert_into_item'      => 'Insert into ' . $lc_singular,
+			'uploaded_to_this_item' => 'Uploaded to this ' . $lc_singular,
+			'items_list'            => ucfirst( $lc_plural ) . ' list',
+			'items_list_navigation' => ucfirst( $lc_plural ) . ' list navigation',
+			'filter_items_list'     => 'Filter ' . $lc_plural . ' list',
 		);
 		return wp_parse_args( $labels, $overrides );
 	}
