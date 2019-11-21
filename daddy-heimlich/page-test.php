074f5e42c6a9d60e3c459f8ca75406b2c8674e7b
Russell Heimlich <kingkool68@gmail.com>
2019-11-20 23:28:43 -0500

    Testing auto deployment.

--- daddy-heimlich/page-test.php
+++ daddy-heimlich/page-test.php
@@ -1,18 +1,6 @@
 <?php
 get_header();
 
-$scraper  = Daddio_Instagram::get_instagram_scraper();
-$location = $scraper->getLocationById( 115578671850891 );
-
-// https://stackoverflow.com/a/27754169/1119655
-function getProtectedValue( $obj, $name ) {
-	$array  = (array) $obj;
-	$prefix = chr( 0 ) . '*' . chr( 0 );
-	return $array[ $prefix . $name ];
-}
-
-var_dump( getProtectedValue( $location, 'data' ) );
-var_dump( $location->getLat() );
-var_dump( $location->getLng() );
+var_dump( 'This was deployed automagically!' );
 
 get_footer();
