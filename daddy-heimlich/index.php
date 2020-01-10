860b459d31190a05418933898de334923f44b54e
Russell Heimlich <kingkool68@gmail.com>
2020-01-09 22:22:50 -0500

    Formatting

--- daddy-heimlich/index.php
+++ daddy-heimlich/index.php
@@ -1,46 +1,46 @@
 <?php
-
 $the_content = array();
 if ( have_posts() ) :
-	while ( have_posts() ) : the_post();
+	while ( have_posts() ) :
+		the_post();
 		$context   = array();
 		$templates = array();
 		switch ( $post->post_type ) {
-			case 'instagram' :
-				$context = array(
-					'title'                 => get_the_title(),
-					'permalink_url'         => get_permalink(),
-					'the_time'              => get_the_time( Daddio_Dates::get_child_time_format() ),
-					'machine_datetime'      => get_post_time( 'c', true ),
-					'child_age'             => Daddio_Dates::how_old_was_child(),
-					'instagram_media'       => Daddio_Media::get_the_instagram_media(),
-					'the_content'           => apply_filters( 'the_content', get_the_content() ),
-					'via_url'               => get_the_guid(),
-					'instagram_username'    => Daddio_Instagram::get_instagram_username(),
+			case 'instagram':
+				$context     = array(
+					'title'              => get_the_title(),
+					'permalink_url'      => get_permalink(),
+					'the_time'           => get_the_time( Daddio_Dates::get_child_time_format() ),
+					'machine_datetime'   => get_post_time( 'c', true ),
+					'child_age'          => Daddio_Dates::how_old_was_child(),
+					'instagram_media'    => Daddio_Media::get_the_instagram_media(),
+					'the_content'        => apply_filters( 'the_content', get_the_content() ),
+					'via_url'            => get_the_guid(),
+					'instagram_username' => Daddio_Instagram::get_instagram_username(),
 				);
 				$templates[] = 'content-instagram.twig';
 				break;
 
-			case 'page' :
-				$context = array(
-					'the_title'             => get_the_title(),
-					'the_content'           => apply_filters( 'the_content', get_the_content() ),
+			case 'page':
+				$context     = array(
+					'the_title'   => get_the_title(),
+					'the_content' => apply_filters( 'the_content', get_the_content() ),
 				);
 				$templates[] = 'content-page.twig';
 				break;
 
-			default :
-				$context = array(
-					'title'                 => get_the_title(),
-					'permalink_url'         => get_permalink(),
-					'the_datetime'          => get_the_time( Daddio_Dates::get_child_time_format() ),
-					'the_machine_datetime'  => get_post_time( 'c', true ),
-					'child_age'             => Daddio_Dates::how_old_was_child(),
-					'the_content'           => apply_filters( 'the_content', get_the_content() ),
+			default:
+				$context     = array(
+					'title'                => get_the_title(),
+					'permalink_url'        => get_permalink(),
+					'the_datetime'         => get_the_time( Daddio_Dates::get_child_time_format() ),
+					'the_machine_datetime' => get_post_time( 'c', true ),
+					'child_age'            => Daddio_Dates::how_old_was_child(),
+					'the_content'          => apply_filters( 'the_content', get_the_content() ),
 				);
 				$templates[] = 'content-post.twig';
 		}
-		$the_content[] =  Sprig::render( $templates, $context );
+		$the_content[] = Sprig::render( $templates, $context );
 	endwhile;
 endif;
 
