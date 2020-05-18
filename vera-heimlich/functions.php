6a95c390531d1c6083b160d9950523a4fcc58f32
Russell Heimlich <kingkool68@gmail.com>
2020-05-18 01:21:16 -0400

    Update Facebook URL for Zadie and Vera

--- vera-heimlich/functions.php
+++ vera-heimlich/functions.php
@@ -4,7 +4,7 @@ define( 'CHILD_DATE_OF_BIRTH', '2017-02-24 12:13PM' );
 define( 'CHILD_NAME', 'Vera' );
 define( 'CHILD_INSTAGRAM_HANDLE', '' );
 define( 'CHILD_INSTAGRAM_HASHTAG', 'VeraAddison' );
-define( 'CHILD_FACEBOOK_URL', 'https://www.facebook.com/media/set/?set=ft.10103094755058458&type=1' );
+define( 'CHILD_FACEBOOK_URL', 'https://www.facebook.com/russellh/media_set?set=ft.10103094755058458&type=1' );
 define( 'CHILD_THEME_COLOR', '#75ac6c' );
 
 $files_to_include = array(
--- zadie-heimlich/functions.php
+++ zadie-heimlich/functions.php
@@ -4,7 +4,7 @@ define( 'CHILD_DATE_OF_BIRTH', '2014-12-28 7:04PM' );
 define( 'CHILD_NAME', 'Zadie' );
 define( 'CHILD_INSTAGRAM_HANDLE', 'lilzadiebug' );
 define( 'CHILD_INSTAGRAM_HASHTAG', 'ZadieAlyssa' );
-define( 'CHILD_FACEBOOK_URL', 'https://www.facebook.com/media/set/?set=ft.10101891838917048&type=1' );
+define( 'CHILD_FACEBOOK_URL', 'https://www.facebook.com/russellh/media_set?set=ft.10101891838917048&type=1' );
 define( 'CHILD_THEME_COLOR', '#7b0e7f' );
 
 $files_to_include = array(
