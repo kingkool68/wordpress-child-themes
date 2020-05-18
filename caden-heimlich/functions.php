9ba892143c241b9f896b4072b3ac5e95f0ddb46b
Russell Heimlich <kingkool68@gmail.com>
2020-05-18 01:18:41 -0400

    Add link to Facebook scrapbook album for Caden

--- caden-heimlich/functions.php
+++ caden-heimlich/functions.php
@@ -4,7 +4,7 @@ define( 'CHILD_DATE_OF_BIRTH', '2020-05-13 01:15PM' );
 define( 'CHILD_NAME', 'Caden' );
 define( 'CHILD_INSTAGRAM_HANDLE', 'CadeWithLove' );
 define( 'CHILD_INSTAGRAM_HASHTAG', 'CadenApollo' );
-define( 'CHILD_FACEBOOK_URL', '' );
+define( 'CHILD_FACEBOOK_URL', 'https://www.facebook.com/russellh/media_set?set=ft.10105569541097398&type=1' );
 define( 'CHILD_THEME_COLOR', '#0f4c81' );
 
 $files_to_include = array(
