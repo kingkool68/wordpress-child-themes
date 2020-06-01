0b26dce72664a450c8c8e5fd3e66ceab242a5a29
Russell Heimlich <kingkool68@gmail.com>
2020-05-31 23:24:28 -0400

    Locally host Google Fonts for Caden Heimlich theme.
    Hoping to improve the performance for the Lighthouse v6 score.

--- caden-heimlich/functions/scripts-styles.php
+++ caden-heimlich/functions/scripts-styles.php
@@ -1,6 +1,6 @@
 <?php
 function cah_wp_enqueue_scripts() {
 	wp_register_style( 'cah-google-fonts', 'https://fonts.googleapis.com/css2?family=Asap:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700&display=swap', array(), null, 'all' );
-	wp_enqueue_style( 'caden-heimlich', get_stylesheet_directory_uri() . '/css/caden-heimlich' . Daddio_Scripts_Styles::get_css_suffix(), array( 'cah-google-fonts' ), null, 'all' );
+	wp_enqueue_style( 'caden-heimlich', get_stylesheet_directory_uri() . '/css/caden-heimlich' . Daddio_Scripts_Styles::get_css_suffix(), array(), null, 'all' );
 }
 add_action( 'wp_enqueue_scripts', 'cah_wp_enqueue_scripts' );
