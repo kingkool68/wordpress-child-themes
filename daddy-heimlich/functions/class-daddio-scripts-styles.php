a97856a8499bae18b643b651bfd5c54e616b6d9d
Russell Heimlich <kingkool68@gmail.com>
2020-06-01 00:41:54 -0400

    Putting JavaScript back as it doesn't seem to make much of a difference.

--- daddy-heimlich/functions/class-daddio-scripts-styles.php
+++ daddy-heimlich/functions/class-daddio-scripts-styles.php
@@ -50,7 +50,7 @@ class Daddio_Scripts_Styles {
 	public function action_wp_enqueue_scripts() {
 		if ( $this->maybe_use_global_script_file() ) {
 			add_filter( 'script_loader_tag', array( $this, 'dont_load_bundled_scripts' ), 10, 3 );
-			// wp_enqueue_script( 'daddio-global-scripts' );
+			wp_enqueue_script( 'daddio-global-scripts' );
 		}
 
 		// Don't load the frontend Block Editor styles
@@ -120,7 +120,7 @@ class Daddio_Scripts_Styles {
 			$new_script_element .= '<!--[if (gte IE 9) | (!IE)]><!-->';
 			$new_script_element .= "<script type='text/javascript' src='" . $jquery3_src . "' defer></script>";
 			$new_script_element .= '<!--<![endif]-->';
-			return '';
+
 			return $new_script_element;
 		}
 
